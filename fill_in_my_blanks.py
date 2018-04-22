9ea788f284efe69b9bf2e21dee50f78e21126e11
Louis Magdaleno <buildthatapp@gmail.com>
2018-04-22 00:00:31 -0700

    update
    added code to functions

--- fill_in_my_blanks.py
+++ fill_in_my_blanks.py
@@ -46,12 +46,32 @@ hard_answers = {
 
 def play_game():
     '''Call functions to play fill in the blank.'''
+def print_stars():
+    '''Print stars across the screen.'''
+    print('*' * banner_length)
 def display_welcome():
     '''Display welcome message to user.'''
+    print_stars()
+    print('')
+    print('Fill in the Blank V1.0')
+    print('A simple game where you select a difficulty level and a number of guesses'+
+        'to fill in a blank.')
+    print('You will then be presented with a text passage containing several missing words.\n' +
+        'You will be asked to enter a word that fills in a missing word. \n'+
+        'Fill all the blanks to win!')
+    print_stars()
 def get_difficulty():
     '''Prompt user to enter difficulty level. Return difficulty level as str.'''
+    diff = input('Please select a game difficulty level. Enter easy, medium, or hard.\n').lower()
+    while diff not in madlibs.keys():
+        diff = input('Enter easy, medium, or hard.\n').lower()
+    return diff
 def get_attempts():
     '''Prompt user to enter number of attempts. Return int > 0.'''
+    attempts = input('How many attempts would you like? Enter a number greater than zero.\n')
+    while attempts <= 0 or not attempts.isdigit():
+        attempts = input('Enter a number greater than zero.\n')
+    return attempts
 def dead_or_alive(att):
     '''Check if dead or alive based on number of attempts left.
     Return bool, True if alive and False is dead.
@@ -59,6 +79,10 @@ def dead_or_alive(att):
     Keyword Arguments:
     att - number of attempts as int
     '''
+    if att == 0:
+        return False
+    else:
+        return True
 def display_madlib(diff, lib_dict):
     '''Dislpay madlib based on difficulty level.
 
@@ -66,26 +90,51 @@ def display_madlib(diff, lib_dict):
     diff - difficulty level as string
     lib_dict - madLib dictionary
     '''
-def get_answer():
-    '''Prompt user to enter answer to blanks. Return answer as str.'''
+    print_stars()
+    print(madlibs[diff])
+    print('')
+def get_answer(blank):
+    '''Prompt user to enter answer to blanks. Return answer as str.
+    
+    Keyword Arguments:
+    blank - the blank word the user is trying to solve, ie (__1__)
+    '''
+    return input('What goes in ' + blank + ' ?\n').lower()
+def check_answer(ans, blank, diff):
+    '''Check a user answer against answers in dictionary. Return True or False.
+
+    Keyword Arguments:å
+    ans - answer entered by user
+    blank - the blank word the user is trying to solve, ie (__1__)
+    diff - difficulty level set by user
+    '''
+    #string name of the dictionary containing answers for the difficulty level the user specified.
+    dictionary = diff + '_answers' 
+    if answer == dictionary[blank]:
+        return True
+    else:
+        return False
+
+
 def update_attempts(att):
     '''Return updated number of attempts after incorrect guess.
     
     Keyword Arguments:
     att - number of attempts as int
     '''
-def update_madlib(lib_dict, answer):
+    return att - 1
+def update_madlib(lib_dict, blank, answer):
     '''Return updated madlib after correct guess.
 
     Keyword Arguments:
     lib_dict - dictionary containing mad lib
+    blank - the blank word the user is trying to solve, ie (__1__)
     answer - correct user answer
     '''
+    t
 def play_again():
     '''Ask user if they want to play again. Return yes or no.'''
 def reset_game():
     '''Reset values of variables to default. '''
-def clear_screen():
-    '''Clear the screen of text.'''
 def test_guessing_game():
     '''Perform tests on guessing game.'''
