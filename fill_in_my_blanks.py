40dd5870e16316eb063bfb19e3b1209d43f39163
Louis Magdaleno <buildthatapp@gmail.com>
2018-04-21 22:48:43 -0700

    update
    defined functions and arguments.
    added docstrings for each function per pep 257.

--- fill_in_my_blanks.py
+++ fill_in_my_blanks.py
@@ -9,14 +9,19 @@
 banner_length = 30
 attempts = 1
 difficulty = 'easy'
-
-
+alive = True
 madlibs = {
-    'easy' : 'Dr. (__1__) is best known for his books The (__2__) in the (__3__) and How the (__4__) stole (__5__).',
-    'medium' : '(__1__) is a (__2__) typed language. It utilizes an (__3__) instead of a compiler. /nThe kind of snake in its logo represents the name of the (__4__)./nIt is a very (__5__) language to type. ',
-    'hard' : 'Jurassic (__1__) was a 1993 movie about (__2__). /nThe plot of the movie was about a dinosaur (__3__) park gone wrong. /nThe final scene pitting a (__4__)-rex against veloci(__5__) was very cool.'
+    'easy' : 'Dr. (__1__) is best known for his books \nThe (__2__)'
+        +'in the (__3__) and How the \n(__4__) stole (__5__).',
+    'medium' : '(__1__) is a (__2__) typed language. It utilizes an\n'
+        +'(__3__) instead of a compiler. \nThe kind of snake in its '
+        +'logo represents the name of the (__4__).\nIt is a very (__5__)'
+        +' language to type. ',
+    'hard' : 'Jurassic (__1__) was a 1993 movie about (__2__).' 
+        +'\nThe plot of the movie was about a dinosaur (__3__) park gone wrong.'
+        +'\nThe final scene pitting a (__4__)-rex against veloci(__5__) was very'
+        +'cool.'
 }
-
 easy_answers = {
     '(__1__)' : 'suess',
     '(__2__)' : 'cat',
@@ -38,70 +43,49 @@ hard_answers = {
     '(__4__)' : 't',
     '(__5__)' : 'raptor'
     }
+
+def play_game():
+    '''Call functions to play fill in the blank.'''
+def display_welcome():
+    '''Display welcome message to user.'''
+def get_difficulty():
+    '''Prompt user to enter difficulty level. Return difficulty level as str.'''
+def get_attempts():
+    '''Prompt user to enter number of attempts. Return int > 0.'''
+def dead_or_alive(att):
+    '''Check if dead or alive based on number of attempts left.
+    Return bool, True if alive and False is dead.
+
+    Keyword Arguments:
+    att - number of attempts as int
+    '''
+def display_madlib(diff, lib_dict):
+    '''Dislpay madlib based on difficulty level.
+
+    Keyword Arguments:
+    diff - difficulty level as string
+    lib_dict - madLib dictionary
+    '''
+def get_answer():
+    '''Prompt user to enter answer to blanks. Return answer as str.'''
+def update_attempts(att):
+    '''Return updated number of attempts after incorrect guess.
     
+    Keyword Arguments:
+    att - number of attempts as int
+    '''
+def update_madlib(lib_dict, answer):
+    '''Return updated madlib after correct guess.
 
-#Select Difficulty
-def select_difficulty():
-    game_difficulty = ''
-    game_difficulty = input('What difficulty would you like to play? \nSelect either easy, medium, or hard.\n').lower()
-    while game_difficulty not in madlibs.keys():
-        difficulty = input('Please enter easy, medium, or hard.\n')
-    print('Okay! \nYour game will be in ' + game_difficulty + ' mode.')
-    print('*' * banner_length)
-    return game_difficulty
-
-
-#Select Number of Attempts
-def select_number_of_attempts():
-    number_of_attempts = int(0)
-    number_of_attempts = input('How many attempts would you like?\nEnter a number greater than 0!.\n')
-    while not number_of_attempts.isdigit() or int(number_of_attempts) < 1:
-        number_of_attempts = input('Please enter a number greater than 0.\n')
-    print('Okay! \nYou will have ' + number_of_attempts + ' attempts to win.')
-    print('*' * banner_length)
-    return number_of_attempts
-
-#Welcome Message
-def welcome_message():
-    print('*' * banner_length)
-    print('Hello! Welcome to Fill In The Blanks V1.0.')
-    print('In this game, you will be asked to choose a \ndifficulty level and a number of guess attempts.')
-    print('After that, you will be given a text passage \nwith several blank words to fill in.')
-    print('*' * banner_length)
-
-#Display Madlib
-def display_madlib(diff):
-    print(madlibs[diff])
-#Ask for Answer
-def ask_for_answer(diff):
-    answer = input('What replaces ' 
-
-#Check Answer
-def check_answer(possible, guess):
-    for answer in possible:
-        if guess.lower() == answer.lower():
-            return True
-    return False
-
-#Answer Question
-def answerquestion(index, quiz):
-        print('in answer question')
-#Unit Tests
-
-#Play Game
-def play_game_func():
-        welcome_message()
-        difficulty = select_difficulty()
-        attempts = select_number_of_attempts()
-        display_madlib(difficulty)
-        print('')
-        print('')
-        print('')
-        print('out of number of attempts')
-
-#Main
-if __name__ == "__main__":
-    play_game = True
-    while play_game == True:
-        play_game_func()
-        
+    Keyword Arguments:
+    lib_dict - dictionary containing mad lib
+    answer - correct user answer
+    '''
+def play_again():
+    '''Ask user if they want to play again. Return yes or no.'''
+def reset_game():
+    '''Reset values of variables to default. '''
+def clear_screen():
+    '''Clear the screen of text.'''
+def test_guessing_game():
+    '''Perform tests on guessing game.'''
